495eb811bd9dae50096cb5afafd41e8d3f3fc431
Becca Petrin <beccapetrin@gmail.com>
2020-01-06 15:53:21 -0800

    add further comments to the interface

--- serviceregistration/service_registration.go
+++ serviceregistration/service_registration.go
@@ -1,5 +1,20 @@
 package serviceregistration
 
+/*
+ServiceRegistration is an interface that can be fulfilled to use
+varying applications for service discovery, regardless of the physical
+back-end used. It uses [the observer pattern](https://refactoring.guru/design-patterns/observer).
+
+Implementing the Factory is essentially how you register your implementation
+as an observer. There is no deregistration because service discovery stops
+on its own if Vault stops.
+
+Service registration implements notifications for changes in _dynamic_
+properties regarding Vault's help. Vault's version is the only static
+property given in state and notifications for now, but if there's a need
+for more in the future, we could add them on.
+ */
+
 import (
 	log "github.com/hashicorp/go-hclog"
 )
@@ -10,6 +25,17 @@ type State struct {
 }
 
 // Factory is the factory function to create a ServiceRegistration.
+//
+// The shutdownCh is the channel to watch for graceful shutdowns _of Vault_,
+// and is great to use for creating background cleanup processes, or for stopping
+// any ongoing goroutines.
+//
+// The config is the key/value pairs set _inside_ the service registration config stanza.
+//
+// The state is only the initial state. The pointer won't be updated over time. All
+// state notifications will come through Notify methods.
+//
+// The redirectAddr is Vault core's RedirectAddr.
 type Factory func(shutdownCh <-chan struct{}, config map[string]string, logger log.Logger, state *State, redirectAddr string) (ServiceRegistration, error)
 
 // ServiceRegistration is an interface that advertises the state of Vault to a
